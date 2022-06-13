7881ed80cde5d44d7f1f368a9b845045c71f5e91
Feng-Yang Hsieh <r10222035@ntu.edu.tw>
2022-06-13 17:01:51 +0800

    Add chisq method and generating tri-Higgs training data

--- SPANet/to_Training_data_triHiggs_b.py
+++ SPANet/to_Training_data_triHiggs_b.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# python to_Training_data_diHiggs_b.py <root file path> <output file name> <minimum b-jet>
+# python to_Training_data_tiiHiggs_b.py <root file path> <output file name> <minimum b-jet>
 
 import uproot
 import numpy as np
@@ -69,9 +69,10 @@ def DeltaR(eta1,phi1, eta2,phi2):
         dPhi = 2*np.pi - dPhi
 
     dR = (dPhi**2 + dEta**2)**0.5
+
     return dR
-    
-def create_diHiggs_dataset_b(f, nevent, MAX_JETS):
+
+def create_triHiggs_dataset_b(f, nevent, MAX_JETS):
     # with b-tagging information
     f.create_dataset('source/mask', (nevent, MAX_JETS), maxshape=(None, MAX_JETS), dtype='|b1')
     f.create_dataset('source/pt', (nevent, MAX_JETS), maxshape=(None, MAX_JETS), dtype='<f4')
@@ -88,6 +89,10 @@ def create_diHiggs_dataset_b(f, nevent, MAX_JETS):
     f.create_dataset('h2/b1', (nevent,), maxshape=(None,), dtype='<i8')
     f.create_dataset('h2/b2', (nevent,), maxshape=(None,), dtype='<i8')
     
+    f.create_dataset('h3/mask', (nevent,), maxshape=(None,), dtype='|b1')
+    f.create_dataset('h3/b1', (nevent,), maxshape=(None,), dtype='<i8')
+    f.create_dataset('h3/b2', (nevent,), maxshape=(None,), dtype='<i8')
+
 def get_particle_mask(quarks_Jet, quarks_index):
     # quarks_index: 粒子對應的夸克編號
     # 若某粒子的 每個夸克都有對應到 jet 且 每個夸克對應的 jet 都沒有重複，則返回 true
@@ -118,7 +123,7 @@ def resize_h5(file_path, nevent):
 def main(file_path, output_name, nbj_min=2, nevent_max=1000000):
     # with b-tagging information
     nbj_min = int(nbj_min)
-
+    
     root_file = uproot.open(file_path)["Delphes;1"]
     GenParticle = BranchGenParticles(root_file)
     
@@ -129,7 +134,7 @@ def main(file_path, output_name, nbj_min=2, nevent_max=1000000):
     jet_Phi = root_file["Jet.Phi"].array()
     jet_Mass = root_file["Jet.Mass"].array()
     jet_BTag = root_file["Jet.BTag"].array()
-
+    
     nevent = min(len(jet_PT), nevent_max)
 
     r = 0.9
@@ -144,8 +149,8 @@ def main(file_path, output_name, nbj_min=2, nevent_max=1000000):
 
     with h5py.File(train_file_path, "w") as f_train, h5py.File(test_file_path, "w") as f_test:
 
-        create_diHiggs_dataset_b(f_train, n_train, MAX_JETS)
-        create_diHiggs_dataset_b(f_test, n_test, MAX_JETS)
+        create_triHiggs_dataset_b(f_train, n_train, MAX_JETS)
+        create_triHiggs_dataset_b(f_test, n_test, MAX_JETS)
 
         for event in tqdm(range(nevent)):
 
@@ -154,10 +159,23 @@ def main(file_path, output_name, nbj_min=2, nevent_max=1000000):
             quarks_id = []
             quarks_Eta = []
             quarks_Phi = []
-            quarks_Jet = [-1,-1,-1,-1]
+            quarks_Jet = [-1,-1,-1,-1,-1,-1]
 
-            # 2 個 Higgs
+            # 找出 3個 final Higgs
+            check_h = []
+            final_h_index = [] 
             for j in np.where(GenParticle.PID_At(event) == 25)[0]:
+                h = j
+                d1 = GenParticle.D1_At(event)[h]
+                while abs(GenParticle.PID_At(event)[d1]) == 25:
+                    h = d1
+                    d1 = GenParticle.D1_At(event)[h]
+                final_h_index.append(h)
+
+            final_h_index = list(set(final_h_index))
+
+            # 找出 6個 final b quark
+            for j in final_h_index:
                 # h > b b~
                 b1 = GenParticle.D1_At(event)[j]
                 b2 = GenParticle.D2_At(event)[j]
@@ -184,14 +202,14 @@ def main(file_path, output_name, nbj_min=2, nevent_max=1000000):
             eta_cut_index = np.array(np.abs(jet_Eta[event])<2.5)
 
             nj = eta_cut_index.astype("int").sum()
+
+            # 至少要 6 jet
+            if nj<6: continue
             
-            # 至少要 4 jet
-            if nj<4: continue
-                
             nbj = np.array(jet_BTag[event][eta_cut_index][:MAX_JETS]).sum()
             # 在前 MAX_JETS jets 中，至少要 nbj_min 個 b-jet
             if nbj<nbj_min: continue
-                
+
             PT = np.array(jet_PT[event][eta_cut_index])
             Eta = np.array(jet_Eta[event][eta_cut_index])
             Phi = np.array(jet_Phi[event][eta_cut_index])
@@ -199,12 +217,17 @@ def main(file_path, output_name, nbj_min=2, nevent_max=1000000):
             BTag = np.array(jet_BTag[event][eta_cut_index])
 
             # 找出每個夸克配對的 jet
+            more_than_1_jet = False
             for quark in range(len(quarks_Jet)):
                 for i in range(min(nj,MAX_JETS)):
                     dR = DeltaR(Eta[i], Phi[i], quarks_Eta[quark], quarks_Phi[quark])
-                    if dR < 0.4 and quarks_Jet[quark] == -1:
+                    if dR<0.4 and quarks_Jet[quark]==-1:
                         quarks_Jet[quark] = i
+                    elif dR<0.4:
+                        more_than_1_jet = True
 
+            if more_than_1_jet: continue
+        
             # 準備寫入資料
             data_dict = {
                 "source/mask": np.arange(MAX_JETS)<nj,
@@ -216,11 +239,14 @@ def main(file_path, output_name, nbj_min=2, nevent_max=1000000):
 
                 "h1/mask": get_particle_mask(quarks_Jet, quarks_index=(0,1)),
                 "h2/mask": get_particle_mask(quarks_Jet, quarks_index=(2,3)),
+                "h3/mask": get_particle_mask(quarks_Jet, quarks_index=(4,5)),
                 
                 "h1/b1": quarks_Jet[0],
                 "h1/b2": quarks_Jet[1],
                 "h2/b1": quarks_Jet[2],
-                "h2/b2": quarks_Jet[3]
+                "h2/b2": quarks_Jet[3],
+                "h3/b1": quarks_Jet[4],
+                "h3/b2": quarks_Jet[5]
             }
 
             if event<n_train:
@@ -232,7 +258,7 @@ def main(file_path, output_name, nbj_min=2, nevent_max=1000000):
                 
     resize_h5(train_file_path, train_index)
     resize_h5(test_file_path, test_index)
-    
+
 if __name__ == '__main__':
     if len(sys.argv)<2: print("No input file.")
         
